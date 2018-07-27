08bcfe45fb3d07223518845d4997f0bc3c434185
vpapako <vpapako@gmail.com>
2018-07-27 13:52:33 +0300

    add support for downloading/sending data, queries and expected resutls

--- src/main/java/org/hobbit/benchmark/versioning/components/VersioningDataGenerator.java
+++ src/main/java/org/hobbit/benchmark/versioning/components/VersioningDataGenerator.java
@@ -1,30 +1,27 @@
 package org.hobbit.benchmark.versioning.components;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.CharSequenceReader;
 import org.apache.commons.lang3.SerializationUtils;
-import org.apache.commons.net.ftp.FTPClient;
 import org.hobbit.benchmark.versioning.Task;
 import org.hobbit.benchmark.versioning.properties.VersioningConstants;
 import org.hobbit.benchmark.versioning.util.SystemAdapterConstants;
@@ -34,7 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Data Generator class for Versioning Benchmark.
+ * Data Generator class for Versioning Benchmark - Large-scale.
  * 
  * @author Vassilis Papakonstantinou (papv@ics.forth.gr)
  *
@@ -48,10 +45,7 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 	private int v0TotalSizeInTriples;
 	private String sentDataForm;
 	private String enabledQueryTypesParam;
-	
-	private String datasetPath = "/versioning/data";
-	private String resultsPath = "/versioning/results";
-	private String queriesPath = "/versioning/queries";
+
 	private String remoteDirectory;
 
 	private int[] triplesExpectedToBeAdded;
@@ -63,7 +57,6 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 
 	private AtomicInteger numberOfmessages = new AtomicInteger(0);
 	
-	private ArrayList<Task> tasks = new ArrayList<Task>();
 	private Semaphore versionLoadedFromSystemMutex = new Semaphore(0);
 	
 	@Override
@@ -74,9 +67,7 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 		// Initialize data generation parameters through the environment variables given by user
 		initFromEnv();
 		
-		remoteDirectory = "https://hobbitdata.informatik.uni-leipzig.de/SPVB-LS/"
-				+ (v0TotalSizeInTriples == 1000000 ? "1M-" : v0TotalSizeInTriples == 5000000 ? "5M-" : v0TotalSizeInTriples == 10000000 ? "10M-" : "TEST_") 
-				+ numberOfVersions + "V";
+		remoteDirectory = "https://hobbitdata.informatik.uni-leipzig.de/SPVB-LS/" + v0TotalSizeInTriples + "T-"+ numberOfVersions + "V";
 		
 		// get the enabled queries 
 		Pattern pattern = Pattern.compile("QT([1-8])=([0|1])[^\\w]*");
@@ -90,17 +81,8 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 
 		// initialize the appropriate tables with the triples that have to be added,
 		// deleted and loaded for each version.
-		getDatasetInfoFromFTP();
-		
-		// TODO: download dataset
-		getDatasetFromFTP();
-		
+		getDatasetInfoFromFTP();		
 
-		// if all query types are disabled skip this part
-		if(!allQueriesDisabled) {
-			// TODO: download queries and expected results
-			getQueriesFromFTP();
-		}
 		LOGGER.info("Data Generator initialized successfully.");
 	}
 	
@@ -147,42 +129,154 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 		} 
 	}
 	
-	public void getDatasetFromFTP() {
-		
+	public void downloadAndSendVersion(int version, String versionRemoteDir) {
+		LOGGER.info("Downloading and sending files of version " + version + "...");		
+		// get the list of files that have to be downloaded
+		String dataFiles = versionRemoteDir + "data_files.txt";
+		String files[] = null;
+		try (InputStream is = new URL(dataFiles).openStream()) {
+			files = IOUtils.toString(is).split("\n");
+		} catch (IOException e) {
+			LOGGER.error("An error occured while reading files to be downloaded for version " + version, e);
+		}
+		// iterate through the list of files,  
+		for (int i = 0; i < files.length; i++) {
+			String graphUri = "http://datagen." + (files[i].contains("added") ? "addset" : "deleteset") + "." + version + "." + FilenameUtils.removeExtension(files[i]);
+			String remoteFile = versionRemoteDir + files[i];
+			LOGGER.info("Downloading file: " + files[i] + "...");
+			try (GZIPInputStream inputStream = (GZIPInputStream) new URL(remoteFile).openStream()) {
+				// de-compress file and send it to system adapter
+				LOGGER.info("Decompressing file " + files[i] + "...");
+				try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+					byte[] buffer = new byte[1024];
+	                int len;
+	                while((len = inputStream.read(buffer)) != -1) {
+	                    out.write(buffer, 0, len);
+	                }
+	                byte[] data = out.toByteArray();
+					byte[] dataForSending = RabbitMQUtils.writeByteArrays(null, new byte[][] { RabbitMQUtils.writeString(graphUri) }, data);
+					sendDataToSystemAdapter(dataForSending);
+				}
+			} catch (IOException e) {
+				LOGGER.error("An error occured while downloading file " + files[i], e);
+			}
+			numberOfmessages.incrementAndGet();
+		}
 	}
 	
-	public void getQueriesFromFTP() {
+	public void downloadAndSendData(int version) {
+		// in case that benchmark is configured to send change-sets 
+		if(sentDataForm.equals("cs") || sentDataForm.equals("both")) {
+			String dataRemoteDir = remoteDirectory + "/data/changesets/c" + version + "/";
+			downloadAndSendVersion(version, dataRemoteDir);
+			LOGGER.info("All files (changesets) of version " + version + " successfully sent to System Adapter.");
+		} else if (sentDataForm.equals("ic") || sentDataForm.equals("both")) {
+			String dataRemoteDir = remoteDirectory + "/data/independentcopies/v" + version + "/";
+			downloadAndSendVersion(version, dataRemoteDir);
+			LOGGER.info("All files (independent copies) of version " + version + " successfully sent to System Adapter.");
+		}
+	}
+	
+	private Task buildTask(int taskId, int queryType, int querySubType, int querySubstitutionParam, String queryFile) {
+		Task task = new Task(Integer.toString(taskId));
+		task.setQueryType(queryType);
+		task.setQuerySubType(querySubType);
+		task.setQuerySubstitutionParam(querySubstitutionParam);
+		
+		// download the query
+		String queryFilePath = remoteDirectory + "/queries/" + queryFile;
+		try (InputStream inputStream = new URL (queryFilePath).openStream()) {
+			task.setQuery(IOUtils.toString(inputStream));
+		} catch (IOException e) {
+			LOGGER.error("An error occured while downloading file " + queryFile, e);
+		}
+		
+		// download the expected results
+		String resultsFile = FilenameUtils.removeExtension(queryFile) + "_results.json.gz";
+		String resultsFilePath = remoteDirectory + "/results/" + resultsFile;
+		try (GZIPInputStream gzipInputStream = (GZIPInputStream) new URL (resultsFilePath).openStream()) {
+			// de-compress file containing the expected results
+			LOGGER.info("Decompressing file " + resultsFile + "...");
+			try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+				byte[] buffer = new byte[1024];
+                int len;
+                while((len = gzipInputStream.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                }
+                task.setExpectedAnswers(out.toByteArray());
+			}
+		} catch (IOException e) {
+			LOGGER.error("An error occured while downloading file " + FilenameUtils.removeExtension(queryFile) + "_results.json.gz", e);
+		}
+		
+		return task;
+	}
+	
+	public void downloadAndSendQueries() {
 		int taskId = 0;
-		int querySubstParamCount = 0;
-		String queriesPath = System.getProperty("user.dir") + File.separator + "queries" + File.separator;
-		String queryString;
 		
-		// QT2
-		if(enabledQueryTypes.getProperty("QT2", "0").equals("1")) {
-			
+		// get the list of all queries
+		String queryFiles = remoteDirectory + "/queries/data_files.txt";
+		String files[] = null;
+		try (InputStream is = new URL(queryFiles).openStream()) {
+			files = IOUtils.toString(is).split("\n");
+		} catch (IOException e) {
+			LOGGER.error("An error occured while reading query files to be downloaded", e);
 		}
 		
-		// QT4
-		// if the total number of versions is lower than 1 there are no historical versions
-		if(enabledQueryTypes.getProperty("QT4", "0").equals("1")) {
+		// iterate through the list of files,  		
+		for (int i = 0; i < files.length; i++) {
+			Task task = null;
+			int queryType = 0;
+			int querySubType = 0; 
+			int querySubstitutionParam = 0;
+					
+			// read the query type, subtype and substparam type
+			Pattern pattern = Pattern.compile("versioningQuery(\\d)\\.(\\d)\\.(\\d)\\.sparql");
+			Matcher matcher = pattern.matcher(files[i]);
+			while (matcher.find()) {
+				queryType = Integer.parseInt(matcher.group(1));
+				querySubType = Integer.parseInt(matcher.group(2));
+				querySubstitutionParam = Integer.parseInt(matcher.group(3));
+			}
 			
-		} 
-		
-		// QT6
-		// same querySubstParamCount as QT5
-		if(enabledQueryTypes.getProperty("QT6", "0").equals("1")) {
-			
-		}
-
-		// QT7
-		// can not be supported when we have 2 or less total versions, as there cannot exist cross-deltas
-		if(enabledQueryTypes.getProperty("QT7", "0").equals("1")) {
-			
-		}
-
-		// QT8
-		if(enabledQueryTypes.getProperty("QT8", "0").equals("1")) {
-			
+			try {
+				switch (queryType) {
+				case 2:
+					if(enabledQueryTypes.getProperty("QT2", "0").equals("1")) {
+						task = buildTask(taskId++, queryType, querySubType, querySubstitutionParam, files[i]);
+						sendDataToTaskGenerator(SerializationUtils.serialize(task));
+					}
+					break;
+				case 4:
+					if(enabledQueryTypes.getProperty("QT4", "0").equals("1")) {
+						task = buildTask(taskId++, queryType, querySubType, querySubstitutionParam, files[i]);
+						sendDataToTaskGenerator(SerializationUtils.serialize(task));
+					}
+					break;
+				case 6:
+					if(enabledQueryTypes.getProperty("QT6", "0").equals("1")) {
+						task = buildTask(taskId++, queryType, querySubType, querySubstitutionParam, files[i]);
+						sendDataToTaskGenerator(SerializationUtils.serialize(task));
+					}
+					break;
+				case 7:
+					if(enabledQueryTypes.getProperty("QT7", "0").equals("1")) {
+						task = buildTask(taskId++, queryType, querySubType, querySubstitutionParam, files[i]);
+						sendDataToTaskGenerator(SerializationUtils.serialize(task));
+					}
+					break;
+				case 8:
+					if(enabledQueryTypes.getProperty("QT8", "0").equals("1")) {
+						task = buildTask(taskId++, queryType, querySubType, querySubstitutionParam, files[i]);
+						sendDataToTaskGenerator(SerializationUtils.serialize(task));
+					}
+					break;
+				}
+			} catch (Exception e) {
+	            LOGGER.error("Exception while sending tasks to Task Generator.", e);
+	        }
+        	LOGGER.info("Task " + task.getTaskId() + " sent to Task Generator.");
 		}
 	}
 	
@@ -220,8 +314,7 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 	
 	
 	@Override
-	// This method is used for sending the already generated data, tasks and gold standard
-	// to the appropriate components.
+	// This method is used for sending data, tasks and expected answers to the appropriate benchmark components.
 	protected void generateData() throws Exception {
 		try {						
 			// Send data files to the system.
@@ -230,42 +323,8 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
 			// loaded, in order to proceed with the next one
 	    	for(int version = 0; version < numberOfVersions; version++) {
 	    		numberOfmessages.set(0);
-	    		// if the benchmark is configured by the system to send change sets
-	    		if(sentDataForm.equals("cs") || sentDataForm.equals("both")) {
-	    			File changesetsPath = new File(datasetPath + "/changesets/c" + version);
-	    			// send the files containing the triples to be added
-	    			List<File> addedDataFiles = (List<File>) FileUtils.listFiles(changesetsPath, new String[] { "added.nt" }, false);
-	    			for (File file : addedDataFiles) {
-	    				String graphUri = "http://datagen.addset." + version + "." + file.getName();
-	    				byte data[] = FileUtils.readFileToByteArray(file);
-	    				byte[] dataForSending = RabbitMQUtils.writeByteArrays(null, new byte[][]{RabbitMQUtils.writeString(graphUri)}, data);
-	    				sendDataToSystemAdapter(dataForSending);
-	    				numberOfmessages.incrementAndGet();
-	    			}
-	    			
-	    			// send the files containing the triples to be deleted	    			
-	    			List<File> deletedDataFiles = (List<File>) FileUtils.listFiles(changesetsPath, new String[] { "deleted.nt" }, false);
-	    			for (File file : deletedDataFiles) {
-	    				String graphUri = "http://datagen.deleteset." + version + "." + file.getName();
-	    				byte data[] = FileUtils.readFileToByteArray(file);
-	    				byte[] dataForSending = RabbitMQUtils.writeByteArrays(null, new byte[][]{RabbitMQUtils.writeString(graphUri)}, data);
-	    				sendDataToSystemAdapter(dataForSending);
-	    				numberOfmessages.incrementAndGet();
-	    			}
-	    		} else if(sentDataForm.equals("ic") || sentDataForm.equals("both")) {
-		    		// if the benchmark is configured by the system to send independent copy of each version.
-	    			File independentcopiesPath = new File(datasetPath + "/independentcopies/v" + version);
-	    			List<File> dataFiles = (List<File>) FileUtils.listFiles(independentcopiesPath, new String[] { "nt" }, false);
-	    			for (File file : dataFiles) {
-	    				String graphUri = "http://datagen.version." + version + "." + file.getName();
-	    				byte data[] = FileUtils.readFileToByteArray(file);
-	    				byte[] dataForSending = RabbitMQUtils.writeByteArrays(null, new byte[][]{RabbitMQUtils.writeString(graphUri)}, data);
-	    				sendDataToSystemAdapter(dataForSending);
-	    				numberOfmessages.incrementAndGet();
-	    			}
-	    		}
+	    		downloadAndSendData(version);
     	    	LOGGER.info("Data for version " + version + " successfully sent to System Adapter.");
-			
             	LOGGER.info("Send signal to benchmark controller that all data (#" + numberOfmessages + ") of version " + version +" successfully sent to system adapter.");
             	ByteBuffer buffer = ByteBuffer.allocate(20);
             	buffer.putInt(triplesExpectedToBeAdded[version]);
@@ -288,17 +347,11 @@ public class VersioningDataGenerator extends AbstractDataGenerator {
             Thread.sleep(1000 * 60);
         }
 		
-        try {
-        	// send generated tasks along with their expected answers to task generator
-        	for (Task task : tasks) {
-        		byte[] data = SerializationUtils.serialize(task);       			
-    			sendDataToTaskGenerator(data);
-            	LOGGER.info("Task " + task.getTaskId() + " sent to Task Generator.");
-        	}
-        	LOGGER.info("All generated tasks successfully sent to Task Generator.");
-        } catch (Exception e) {
-            LOGGER.error("Exception while sending tasks to Task Generator.", e);
-        }
+		// if all queries are disabled skip this part
+		if(!allQueriesDisabled) {
+			downloadAndSendQueries();
+			LOGGER.info("All generated tasks successfully sent to Task Generator.");
+		}
 	}
 	
 	@Override
